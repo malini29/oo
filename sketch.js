ecc226384a12ba01882b55e3142619a53b309a47
malini29 <68008403+malini29@users.noreply.github.com>
2020-09-03 07:23:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var log1,log2,log3
+
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -39,25 +39,30 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
 
 	Engine.run(engine);
-	
-boxPosition=width/2-100 
-boxY=610;
- boxleftSprite=createSprite(boxPosition, boxY, 20,100)
-  boxleftSprite.shapeColor=color(255,0,0);
-   boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
-    World.add(world, boxLeftBody);
-	 boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
-	  boxBase.shapeColor=color(255,0,0);
-	   boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
-	    World.add(world, boxBottomBody);
-		 boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
-		  boxleftSprite.shapeColor=color(255,0,0); 
-		  boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
-		   World.add(world, boxRightBody);
-
-    
+  
 }
 
 
@@ -67,29 +72,26 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
- 
-
-
-  
   drawSprites();
  
 }
 
-function keyPressed() { 
-	if (keyCode === LEFT_ARROW) { 
-		helicopterSprite.x=helicopterSprite.x-20;
-		 translation={x:-20,y:0}
-		  Matter.Body.translate(packageBody, translation)
-		   } 
-		   else if (keyCode === RIGHT_ARROW) { 
-			   helicopterSprite.x=helicopterSprite.x+20; 
-			   translation={x:20,y:0}
-			    Matter.Body.translate(packageBody, translation)
-				 } 
-				 else if (keyCode === DOWN_ARROW) { 
-					 Matter.Body.setStatic(packageBody,false);
-					  } 
-					  }
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+
+    helicopterSprite.x=helicopterSprite.x-20;    
+    translation={x:-20,y:0}
+    Matter.Body.translate(packageBody, translation)
 
 
+  } else if (keyCode === RIGHT_ARROW) {
+    helicopterSprite.x=helicopterSprite.x+20;
+    translation={x:20,y:0}
+    Matter.Body.translate(packageBody, translation)
+  }
+  else if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
 
